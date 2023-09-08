274527e4b7f4c7d86d09e88a1aa5d8d7d61ac935
chaitanya sharma <100488277+cs223441@users.noreply.github.com>
2023-09-08 10:31:07 +0530

    Add files via upload

---
+++ coding.cpp
@@ -0,0 +1,21 @@
+/*Class:it is the datatype in c++ which can have objects(data members , member functions) 
+etc vehical --> class and it's objects: mercedeze,alto,kia,scorpio etc , lets see and example below....
+
+syntax: class[keyword] class name{
+
+}
+*/
+//Eg
+
+class car{
+cout<<'this is car';
+}
+ 
+//Object
+//data member
+//member function
+//structures in C++
+//access specifiers
+//defining functino inside and outside the class
+//array of objects
+
